c8f5219b167a6fd4d80e7b0f93bd0efa8289e5de
weird-trust <robert.burtzik@gmail.com>
2025-05-16 15:31:54 +0200

    feat: adjust images and tags

--- src/lib/projects/projects.ts
+++ src/lib/projects/projects.ts
@@ -29,13 +29,13 @@ export const projects: Project[] = [
             '2024'
         ],
         images: [
-            '/images/project1/01_AnsichtsSachen.jpg',
-            '/images/project1/02_AnsichtsSachen.jpg',
-            '/images/project1/03_AnsichtsSachen.jpg',
-            '/images/project1/04_AnsichtsSachen.jpg',
-            '/images/project1/05_AnsichtsSachen.jpg',
-            '/images/project1/06_AnsichtsSachen.jpg',
-            '/images/project1/07_AnsichtsSachen.jpg'
+            '/images/project1/01_AnsichtsSachen.webp',
+            '/images/project1/02_AnsichtsSachen.webp',
+            '/images/project1/03_AnsichtsSachen.webp',
+            '/images/project1/04_AnsichtsSachen.webp',
+            '/images/project1/05_AnsichtsSachen.webp',
+            '/images/project1/06_AnsichtsSachen.webp',
+            '/images/project1/07_AnsichtsSachen.webp'
         ]
     },
     {
@@ -57,14 +57,14 @@ export const projects: Project[] = [
             '2023'
         ],
         images: [
-            '/images/project2/01_DigitaleReizberflutung.jpg',
-            '/images/project2/02_DigitaleReizberflutung.jpg',
-            '/images/project2/03_DigitaleReizberflutung.jpg',
-            '/images/project2/04_DigitaleReizberflutung.jpg',
-            '/images/project2/05_DigitaleReizberflutung.jpg',
-            '/images/project2/06_DigitaleReizberflutung.jpg',
-            '/images/project2/07_DigitaleReizberflutung.jpg',
-            '/images/project2/08_DigitaleReizberflutung.jpg'
+            '/images/project2/01_DigitaleReizberflutung.webp',
+            '/images/project2/02_DigitaleReizberflutung.webp',
+            '/images/project2/03_DigitaleReizberflutung.webp',
+            '/images/project2/04_DigitaleReizberflutung.webp',
+            '/images/project2/05_DigitaleReizberflutung.webp',
+            '/images/project2/06_DigitaleReizberflutung.webp',
+            '/images/project2/07_DigitaleReizberflutung.webp',
+            '/images/project2/08_DigitaleReizberflutung.webp'
         ]
     },
     {
@@ -75,13 +75,13 @@ export const projects: Project[] = [
         description: '12 Collagen der Tierkreiszeichen in analoger Ausführung.',
         details: ['Konzept', 'Illustration', 'Fotografie', 'Editorial Design', '2021/2022'],
         images: [
-            '/images/project3/01_Zodiacs.jpg',
-            '/images/project3/02_Zodiacs.jpg',
-            '/images/project3/03_Zodiacs.jpg',
-            '/images/project3/04_Zodiacs.jpg',
-            '/images/project3/05_Zodiacs.jpg',
-            '/images/project3/06_Zodiacs.jpg',
-            '/images/project3/07_Zodiacs.jpg'
+            '/images/project3/01_Zodiacs.webp',
+            '/images/project3/02_Zodiacs.webp',
+            '/images/project3/03_Zodiacs.webp',
+            '/images/project3/04_Zodiacs.webp',
+            '/images/project3/05_Zodiacs.webp',
+            '/images/project3/06_Zodiacs.webp',
+            '/images/project3/07_Zodiacs.webp'
         ]
     },
     {
@@ -93,9 +93,9 @@ export const projects: Project[] = [
             'Neukonzeption des Color InSight Trendfarben-Fächers. Der "VIEW WHAT\'S NEXT ColorInsight"-Fächer erscheint quartalsweise. Erstellt im Auftrag von LEONHARD KURZ Stiftung & Co. KG.',
         details: ['Konzept', 'Editorial Design', '9 Seiten', '10 x 10,5 cm', '2024'],
         images: [
-            '/images/project4/image1.jpg',
-            '/images/project4/image2.jpg',
-            '/images/project4/image3.jpg'
+            '/images/project4/image1.webp',
+            '/images/project4/image2.webp',
+            '/images/project4/image3.webp'
         ]
     },
     {
@@ -107,11 +107,11 @@ export const projects: Project[] = [
             'Konzept und Design eines fiktiven alkoholfreien Gins in zwei Varianten (Heiß- und Kaltprägeverfahren).\nErstellt im Auftrag von LEONHARD KURZ Stiftung & Co. KG.',
         details: ['Konzept', 'Grafikdesign', 'Fotografien: Thomas Riese', '2023'],
         images: [
-            '/images/project5/image1.jpg',
-            '/images/project5/image2.jpg',
-            '/images/project5/image3.jpg',
-            '/images/project5/image4.jpg',
-            '/images/project5/image5.jpg'
+            '/images/project5/image1.webp',
+            '/images/project5/image2.webp',
+            '/images/project5/image3.webp',
+            '/images/project5/image4.webp',
+            '/images/project5/image5.webp'
         ]
     },
     {
@@ -132,15 +132,15 @@ export const projects: Project[] = [
             '2023/2024'
         ],
         images: [
-            '/images/project6/image1.jpg',
-            '/images/project6/image2.jpg',
-            '/images/project6/image3.jpg',
-            '/images/project6/image4.jpg',
-            '/images/project6/image5.jpg',
-            '/images/project6/image6.jpg',
-            '/images/project6/image7.jpg',
-            '/images/project6/image8.jpg',
-            '/images/project6/image9.jpg'
+            '/images/project6/image1.webp',
+            '/images/project6/image2.webp',
+            '/images/project6/image3.webp',
+            '/images/project6/image4.webp',
+            '/images/project6/image5.webp',
+            '/images/project6/image6.webp',
+            '/images/project6/image7.webp',
+            '/images/project6/image8.webp',
+            '/images/project6/image9.webp'
         ]
     },
     {
@@ -151,6 +151,6 @@ export const projects: Project[] = [
         description:
             'Konzept und Design eines Kooperationslogos zwischen Pentawards und LEONHARD KURZ Stiftung & Co. KG für die Pentawards Veranstaltung 2023.\nErstellt im Auftrag von LEONHARD KURZ Stiftung & Co. KG.',
         details: ['Konzept', 'Grafikdesign', '2023'],
-        images: ['/images/project7/image1.jpg']
+        images: ['/images/project7/image1.webp']
     }
 ];
